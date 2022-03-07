e031856c7e1c4e5a087288a9343f0405e73091e3
Phil <phil.ayres@consected.com>
2022-03-07 18:53:44 +0000

    Added options for in_specific_record and to_existing_record

--- app/models/save_triggers/create_reference.rb
+++ app/models/save_triggers/create_reference.rb
@@ -1,27 +1,7 @@
 # frozen_string_literal: true
 
 class SaveTriggers::CreateReference < SaveTriggers::SaveTriggersBase
-  def self.config_def(if_extras: {})
-    # [
-    #   {
-    #     model_name: {
-    #       if: if_extras,
-    #       in: 'this | referring_record | master (creates no reference, just uses master_id) | master_with_reference (creates a reference to the master, not the item)',
-    #       force_create: 'true to force the creation of a reference and referenced object, independent of user access controls',
-    #       with: {
-    #         field_name: 'now()',
-    #         field_name_2: 'literal value',
-    #         field_name_3: {
-    #           this: 'field_name'
-    #         },
-    #         field_name_4: {
-    #           reference_name: 'field_name'
-    #         }
-    #       }
-    #     }
-    #   }
-    # ]
-  end
+  def self.config_def(if_extras: {}); end
 
   def initialize(config, item)
     super
@@ -35,15 +15,20 @@ class SaveTriggers::CreateReference < SaveTriggers::SaveTriggersBase
     @model_defs.each do |model_def|
       model_def.each do |model_name, config|
         vals = {}
+        force_create = config[:force_create]
+        to_existing_record = config[:to_existing_record]
+        create_in = config[:in]
+        create_if = config[:if]
+        create_with = config[:with]
 
         # We calculate the conditional if inside each item, rather than relying
         # on the outer processing in ActivityLogOptions#calc_save_trigger_if
-        if config[:if]
-          ca = ConditionalActions.new config[:if], @item
+        if create_if
+          ca = ConditionalActions.new create_if, @item
           next unless ca.calc_action_if
         end
 
-        config[:with]&.each do |fn, def_val|
+        create_with&.each do |fn, def_val|
           if def_val.is_a? Hash
             ca = ConditionalActions.new def_val, @item
             res = ca.get_this_val
@@ -55,19 +40,54 @@ class SaveTriggers::CreateReference < SaveTriggers::SaveTriggersBase
         end
 
         @item.transaction do
-          force_create = config[:force_create]
-          new_item = @master.assoc_named(model_name.to_s.pluralize).new vals
-          new_item.force_save! if force_create
-          new_item.save!
+          new_type = @master.assoc_named(model_name.to_s.pluralize)
+          if to_existing_record
+            to_existing_record_id = to_existing_record[:record_id]
+            if to_existing_record_id.is_a? Hash
+              ca = ConditionalActions.new to_existing_record_id, @item
+              to_existing_record_id = ca.get_this_val
+            end
 
-          if config[:in] == 'this'
+            new_item = new_type.find(to_existing_record_id)
+          else
+            new_item = new_type.new vals
+            new_item.save!
+            new_item.force_save! if force_create
+          end
+
+          case create_in
+          when 'this'
             ModelReference.create_with @item, new_item, force_create: force_create
-          elsif config[:in] == 'referring_record'
+          when 'referring_record'
             ModelReference.create_with @item.referring_record, new_item, force_create: force_create
-          elsif config[:in] == 'master'
+          when 'master'
             # ModelReference.create_from_master_with @master, new_item
-          elsif config[:in] == 'master_with_reference'
+          when 'master_with_reference'
             ModelReference.create_from_master_with @master, new_item, force_create: force_create
+          when 'specific_record'
+            in_specific_record = config[:in_specific_record]
+            in_record_type = in_specific_record[:record_type]
+            in_record_id = in_specific_record[:record_id]
+            in_master_id = in_specific_record[:master_id]
+
+            if in_record_id.is_a? Hash
+              ca = ConditionalActions.new in_record_id, @item
+              in_record_id = ca.get_this_val
+            end
+
+            if in_master_id.is_a? Hash
+              ca = ConditionalActions.new in_master_id, @item
+              in_master_id = ca.get_this_val
+            end
+
+            if in_record_type
+              model = Resources::Models.find_by(resource_name: in_record_type).model
+              create_in_item = model.find(in_record_id)
+            else
+              create_in_item = Master.find(in_master_id)
+            end
+
+            ModelReference.create_with create_in_item, new_item, force_create: force_create
           else
             raise FphsException, "Unknown 'in' value in create_reference"
           end
