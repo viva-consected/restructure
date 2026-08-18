59705496544e3f79476efa0dcc273bd229c80962
Restructure Build Process <viva-toolbox@project-viva>
2026-08-18 08:54:45 +0000

    Built and tested release-ready version '10.0.4' - dev repo

--- db/dumps/current_schema.sql
+++ db/dumps/current_schema.sql
@@ -3,7 +3,7 @@ begin;
 -- PostgreSQL database dump
 --
 
-\restrict G1VD5xzlWQRLpmMqIdRDUtWCwxUIJ4UbOnBYum8XWoB4WhwmNMsbH1mtcZglZKZ
+\restrict qUw3QIOnPqkaVTquY7NJMUElqEveaVo6RmSngf27ploDZhHVvw82IDsqDrMSA60
 
 -- Dumped from database version 15.18
 -- Dumped by pg_dump version 15.18
@@ -23385,6 +23385,6 @@ ALTER TABLE ONLY ref_data.redcap_data_dictionary_history
 -- PostgreSQL database dump complete
 --
 
-\unrestrict G1VD5xzlWQRLpmMqIdRDUtWCwxUIJ4UbOnBYum8XWoB4WhwmNMsbH1mtcZglZKZ
+\unrestrict qUw3QIOnPqkaVTquY7NJMUElqEveaVo6RmSngf27ploDZhHVvw82IDsqDrMSA60
 
 commit;
